75541ba6c31bfd00c6cdf191bc64025e9e111af2
Jamal Hasuev <metafra@mail.ru>
2021-07-17 19:06:41 +0300

    remove Game code to App

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,63 @@
 <template>
-  <Game />
+  <div v-if="downloaded" class="game-container">
+    <navbar />
+    <div :id="containerId" class="game-container__inner" />
+  </div>
+  <div v-else class="preload-text">
+    Игра загружается ...
+  </div>
 </template>
 
 <script>
-import Game from '@/components/Game'
+import Navbar from "./components/Navbar"
 export default {
   name: "App",
-  components: { Game }
+  components: {
+    Navbar,
+  },
+  data() {
+    return {
+      downloaded: false,
+      gameInstance: null,
+      containerId: 'game-container'
+    }
+  },
+  async mounted() {
+    const game = await import(/* webpackChunkName: "game" */ '@/game/game')
+    this.downloaded = true
+    this.$nextTick(() => {
+      this.gameInstance = game.launch(this.containerId)
+    })
+  },
+  destroyed() {
+    this.gameInstance.destroy(false)
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+
+.game-container {
+  height: 100vh;
+  width: 100vw;
+  padding: 100px 15px;
+
+  &__inner {
+    width: 100%;
+    height: 100%;
+  }
+}
+
+.preload-text {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  height: 100vh;
+  width: 100vw;
+
+  font-size: 2rem;
+  font-family: 'Courier New', Courier, monospace;
+}
+
+</style>
