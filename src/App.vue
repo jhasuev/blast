0328b84037ebdc70fb9ef406ae029f2d37a0a877
Jamal Hasuev <metafra@mail.ru>
2021-07-18 03:15:48 +0300

    popup styling

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div v-if="downloaded" class="game-container">
     <div ref="ui">
       <navbar />
-      <popup v-model="popup" :bgClick="!false">
-        Popup template
+      <popup v-model="popup" title="Меню">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita alias porro aliquam quos vero voluptatem voluptas rerum unde quibusdam natus, praesentium aspernatur incidunt libero maxime exercitationem magni facere eum! Aspernatur!
       </popup>
     </div>
     <div :id="containerId" class="game-container__inner" />
--- src/components/common/Popup.vue
+++ src/components/common/Popup.vue
@@ -3,7 +3,12 @@
     <div v-if="modelValue" class="popup">
       <i class="popup__bg" @click="onBackgroundClick()" />
 
-      <slot />
+      <div class="popup__inner">
+        <h3 v-if="title" v-text="title" class="popup__title" />
+        <div class="popup__content">
+          <slot />
+        </div>
+      </div>
     </div>
   </transition>
 </template>
@@ -13,6 +18,7 @@ export default {
   name: "Popup",
   props: {
     modelValue: { type: Boolean },
+    title: { type: String, default: "" },
     bgClick: { type: Boolean, default: true },
   },
   methods: {
@@ -48,6 +54,70 @@ export default {
     bottom: 0;
     z-index: -1;
     background: rgba(0,0,0,.5);
+  }
+
+  &__inner {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    min-width: 290px;
+    max-width: 320px;
+  }
+
+  &__title {
+    position: relative;
+    z-index: 1;
+
+    min-width: 174px;
+    padding: 10px 15px;
+
+    align-self: center;
+    min-height: 40px;
+    font-size: 22px;
+
+    background: linear-gradient(to bottom, #98CEFF, #004F81);
+    border-radius: 14px;
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+
+    &:after {
+      content: "";
+      position: absolute;
+      z-index: -1;
+      top: 3px;
+      left: 3px;
+      right: 3px;
+      bottom: 0;
+
+      background: linear-gradient(to top, #00306B 10%, #00306B 20%, #00D5FE 120%);
+      border-radius: inherit;
+    }
+  }
+
+  &__content {
+    position: relative;
+    z-index: 1;
+
+    padding: 13px 32px 17px;
+
+    background-color: #00539E;
+    border: 7px solid #00D2EF;
+    border-radius: 17px;
+
+    box-shadow: 0 5px 5px rgba(0,0,0,.25);
+
+    &:after {
+      content: "";
+      position: absolute;
+      z-index: -1;
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      border-radius: 10px;
+
+      box-shadow: 0 0 5px 2px rgba(0,0,0,.8), inset 0 0 6px 2px rgba(255,255,255,.35);
+    }
   }
 }
 
